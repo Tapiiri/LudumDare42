5e64c74bca8996e02a49c7a16ffb71a7fdc38095
Ilmari Tarpila <leo.tarpila@aalto.fi>
2018-08-11 11:56:30 +0300

    Player to center screen, bigger sway

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
-const KEYCODE_ENTER = 13;  	//useful keycode
-const KEYCODE_SPACE = 32;	//useful keycode
-const KEYCODE_UP    = 38;   //useful keycode
-const KEYCODE_LEFT  = 37;	//useful keycode
-const KEYCODE_RIGHT = 39;	//useful keycode
-const KEYCODE_DOWN  = 40;
-const KEYCODE_W     = 87;	//useful keycode
-const KEYCODE_A     = 65;	//useful keycode
-const KEYCODE_D     = 68;	//useful keycode
+const KEYCODE_ENTER = 13; //useful keycode
+const KEYCODE_SPACE = 32; //useful keycode
+const KEYCODE_UP = 38; //useful keycode
+const KEYCODE_LEFT = 37; //useful keycode
+const KEYCODE_RIGHT = 39; //useful keycode
+const KEYCODE_DOWN = 40;
+const KEYCODE_W = 87; //useful keycode
+const KEYCODE_A = 65; //useful keycode
+const KEYCODE_D = 68; //useful keycode
 
 document.onkeydown = handleKeyDown;
 document.onkeyup = handleKeyUp;
@@ -19,12 +19,12 @@ function init() {
   stage.w;
   let player = new createjs.Shape();
   player.graphics.beginFill('Red').drawRect(0, 0, 100, 100);
-  player.x = 100;
+  player.x = $(window).width() / 2;
   player.y = 100;
   stage.addChild(player);
   createjs.Ticker.addEventListener('tick', onTick);
   function onTick(ev) {
-    player.x += 5 * Math.sin(0.005 * ev.time);
+    player.x += 5 * Math.sin(0.0005 * ev.time);
     stage.update();
   }
 
@@ -32,33 +32,33 @@ function init() {
 }
 
 function handleKeyDown(e) {
-	if (!e) {
-		var e = window.event;
-	}
-	switch (e.keyCode) {
-		case KEYCODE_LEFT:
-			return false;
-		case KEYCODE_RIGHT:
-			return false;
-		case KEYCODE_UP:
+  if (!e) {
+    var e = window.event;
+  }
+  switch (e.keyCode) {
+    case KEYCODE_LEFT:
+      return false;
+    case KEYCODE_RIGHT:
+      return false;
+    case KEYCODE_UP:
       return false;
     case KEYCODE_DOWN:
-			return false;
-	}
+      return false;
+  }
 }
 
 function handleKeyUp(e) {
-	if (!e) {
-		var e = window.event;
-	}
-	switch (e.keyCode) {
-		case KEYCODE_LEFT:
-			return false;
-		case KEYCODE_RIGHT:
-			return false;
-		case KEYCODE_UP:
+  if (!e) {
+    var e = window.event;
+  }
+  switch (e.keyCode) {
+    case KEYCODE_LEFT:
+      return false;
+    case KEYCODE_RIGHT:
+      return false;
+    case KEYCODE_UP:
       return false;
     case KEYCODE_DOWN:
-			return false;
-	}
+      return false;
+  }
 }
