f2ad7e9f624ee35fa01f1e89ee073116e18e27be
Ilmari Tarpila <leo.tarpila@aalto.fi>
2018-08-11 11:22:07 +0300

    Whoops again

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ function init() {
   let player = new createjs.Shape();
   player.graphics.beginFill('Red').drawRect(0, 0, 100, 100);
   player.x = 100;
-  circle.y = 100;
+  player.y = 100;
   stage.addChild(player);
   stage.update();
 
