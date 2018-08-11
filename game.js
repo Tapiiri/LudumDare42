ff908487b6548f39f0a689b1b07242de9095e5fb
Juho Rinta-Paavola <juho.s.rp@gmail.com>
2018-08-11 11:58:23 +0300

    Add gameObject type

--- game.js
+++ game.js
@@ -1,13 +1,31 @@
 function init() {
   let stage = new createjs.Stage('demoCanvas');
-  let player = new createjs.Shape();
-  player.graphics.beginFill('Red').drawRect(0, 0, 100, 100);
-  player.x = 100;
-  player.y = 100;
-  stage.addChild(player);
+  /* gameObject contains:
+   *   graphics - EaselJS DisplayObject
+   *   onTick - function (tick event, this gameObject)
+   *   gravity - Boolean whether to apply gravity
+   *   hitbox - ???
+   */
+  const gameObjects = [];
+  function addGameObject(go) {
+    stage.addChild(go.graphics);
+    gameObjects.push(go);
+  }
+
+  const playerGraphics = new createjs.Shape();
+  playerGraphics.graphics.beginFill('Red').drawRect(0, 0, 100, 100);
+  playerGraphics.x = 100;
+  playerGraphics.y = 100;
+  const player = {
+    graphics: playerGraphics,
+    onTick: (ev, self) => self.graphics.x += 5 * Math.sin(0.005 * ev.time),
+    gravity: true,
+  };
+  addGameObject(player);
+
   createjs.Ticker.addEventListener('tick', onTick);
   function onTick(ev) {
-    player.x += 5 * Math.sin(0.005 * ev.time);
+    gameObjects.forEach(go => go.onTick(ev, go));
     stage.update();
   }
 
